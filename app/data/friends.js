8f2c6f68554c320e31cd09e517efb85cbe170ce3
Daniel Rosana <danielrosana@Daniel-Rosanas-MacBook-Pro.local>
2017-09-19 19:26:55 -0400

    working

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+//  Friends data
+
+var friends = [ {
+  "name" : "Henrik",
+  "photo" : "https://nhl.bamcontent.com/images/headshots/current/168x168/8468685.jpg",
+  "scores" : [5,4,3,3,2,3,5,1,4,1]
+}, {
+  "name" : "Lias",
+  "photo" : "http://cdn.newsday.com/polopoly_fs/1.13799811.1499980944!/httpImage/image.jpeg_gen/derivatives/display_600/image.jpeg",
+  "scores" : [3,5,2,3,1,2,1,4,2,5]
+}, {
+  "name" : "Rick",
+  "photo" : "http://www.hockeydb.com/ihdb/photos/rick-nash-2017-48.jpg",
+  "scores" : [2,3,1,2,4,5,5,2,3,5]
+}, {
+  "name" : "Ryan",
+  "photo" : "http://www.hockeydb.com/ihdb/photos/ryan-mcdonagh-2017-48.jpg",
+  "scores" : [2,1,4,3,2,1,4,5,2,1]
+}, {
+  "name" : "Filip",
+  "photo" : "http://www2.pictures.zimbio.com/gi/Filip+Chytil+2017+NHL+Draft+Portraits+SS-O6REM1NMl.jpg",
+  "scores" : [2,3,4,2,1,3,5,4,3,2]
+},  {
+  "name" : "Pavel",
+  "photo" : "https://nhl.bamcontent.com/images/headshots/current/168x168/8477402.jpg",
+  "scores" : [4,5,2,3,1,3,4,2,1,5]
+}];
+
+
+module.exports = friends;
